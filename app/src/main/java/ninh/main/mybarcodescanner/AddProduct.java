59dc21d05b00ab84e21991e5683ca95326158a75
LND <nghiadung.work@gmail.com>
2023-11-08 17:52:23 +0700

    lnd - DatabaseHelper.java and DBManager.java lnd - manifest lnd - manifest and addProduct

--- app/src/main/java/ninh/main/mybarcodescanner/AddProduct.java
+++ app/src/main/java/ninh/main/mybarcodescanner/AddProduct.java
@@ -3,10 +3,12 @@ package ninh.main.mybarcodescanner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -82,9 +84,17 @@ public class  AddProduct extends AppCompatActivity {
         Intent main = new Intent(this, ProductListActivity.class)
                 .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(main);
+//        closeKeyboard();
         }
 //        this.returnHome();
 
+//    private void closeKeyboard(){
+//        View view = this.getCurrentFocus();
+//        if(view != null){
+//            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//            imm.hideSoftInputFromWindow(view.getWindowToken(),0);
+//        }
+//    }
     public void returnHome() {
         Intent home_intent = new Intent(getApplicationContext(), ProductListActivity.class)
                 .setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
